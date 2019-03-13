7460fb3c059158f04be99da6eaf23e74959d03fb
bea <bianca.subion@gmail.com>
2019-03-12 18:40:10 -0700

    [instagram] initialize

---
+++ app/components/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link } from 'react-router';
+import App from './App/index.jsx';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
+
